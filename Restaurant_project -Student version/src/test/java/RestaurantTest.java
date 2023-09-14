98199d40b5bf3734e53115dfb16e9ebfe78e35ad
sriharshapaladugu <143207255+sriharshapaladugu@users.noreply.github.com>
2023-09-14 22:18:36 +0530

    Add files via upload

---
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -0,0 +1,100 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeEach
+    public void initiateRestorentObject(){
+
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        restaurant = Mockito.spy(restaurant);
+    }
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        Mockito.when(restaurant.isRestaurantOpen()).thenReturn(true);
+
+        assertTrue(restaurant.isRestaurantOpen());
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        Mockito.when(restaurant.isRestaurantOpen()).thenReturn(false);
+
+        assertFalse(restaurant.isRestaurantOpen());
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //TDD
+    /*
+        Implement a method that returns OrderValue given list of items from the menu
+        -> parameters passed list of names of items
+        -> method should
+            1. search for the item in menu list
+            2. get its price
+            3. add to orderTotal
+            4. return orderTotal
+
+        -> the menu will be specific to restaurant so method can be part of Restaurant class
+
+    * */
+
+    // Passing test case
+    //<<<<<<<<<<<<<<<<<<<<<<<ORDER TOtal>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void orader_total_for_selected_menu_items_should_equals_388(){
+
+        // form the selected item list
+        List<String> selectedItems = new ArrayList<>();
+        selectedItems.add("Sweet corn soup");
+        selectedItems.add("Vegetable lasagne");
+
+        int orderTotal = restaurant.getOrderTotal(selectedItems);
+
+        assertEquals(388, orderTotal);
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<ORDER TOtal>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
